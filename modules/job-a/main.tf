020e380381a3e615c6dd6cec03fa70bb24193826
Aksel Skaar Leirvaag <52233080+akselleirv@users.noreply.github.com>
2023-02-18 16:12:51 +0100

    Update main.tf

--- modules/job-a/main.tf
+++ modules/job-a/main.tf
@@ -1,15 +1,1 @@
-output "my_list" {
-  value = [
-    "entry-1",
-    "entry-2"
-  ]
-}
-
-output "complex" {
-  value = {
-    inner = {
-      array  = ["hello", "world"]
-      number = 42
-    }
-  }
-}
+resource "null_resource" "debug" {}
